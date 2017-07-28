3c532092768f1c67165d902bc975739f5ef8bded
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2017-07-27 20:28:04 -0400

    Organize files

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  SpriteBlasta
+//
+//  Created by Jeff Kereakoglow on 7/27/17.
+//  Copyright © 2017 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
